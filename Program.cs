b72a2491a8b042ced937500fca79c63cbc540902
olesya <asiyush@gmail.com>
2022-09-11 23:13:35 +0300

    dz4

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+// void Numbers(){
+// int result = 1;
+// Console.WriteLine("Введите число A: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// for (int i=1; i <=n; i++)
+//     {
+//     result *= num  ;
+//      }
+//      Console.WriteLine($"Число A в степени B равно {result}");
+
+
+// }
+// Numbers();
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// void Numbers(){
+
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// int a = num % 10;
+//             while (num != 0)
+//             {
+//                  num = num /10 ;
+//                 sum = sum + num % 10;
+               
+//                      }
+// sum = sum + a;                     
+
+//  Console.WriteLine($"Сумма цифр в числе равна {sum}");
+
+// }
+// Numbers();
+
+
+// Задача 29: Напишите программу, которая задаёт массив из произвольного элементов и выводит их на экран.
+
+ 
+// int[] arr =  new int[8];
+// int size = 8;
+// int i = 0;
+// Console.WriteLine(" Введите числа: ");
+// Console.ReadLine();
+//        for (i = 0; i < size; i++ )
+//     {
+//     Console.WriteLine(arr[ i ] + " " );
+// Console.ReadLine();
+// }
